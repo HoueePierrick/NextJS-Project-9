7a995014b64107e97d763cf0a608a7a74e00dee4
PierrickHOUEE <houee.pierrick@gmail.com>
2023-04-11 19:43:05 +0200

    WIP

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,23 +1,23 @@
-import classes from './comment-list.module.css';
+import classes from "./comment-list.module.css";
 
-function CommentList() {
+function CommentList(props) {
+  const { items } = props;
   return (
     <ul className={classes.comments}>
       {/* Render list of comments - fetched from API */}
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
+      {items.map((e, i) => {
+        return (
+          <li key={e.id}>
+            <p>{e.text}</p>
+            <div>
+              By <address>{e.name}</address>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
 
 export default CommentList;
+// mongodb+srv://phouee:<password>@clustertestph.lyjon.mongodb.net/?retryWrites=true&w=majority
